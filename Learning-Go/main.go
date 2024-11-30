fa4920b058eb9fbcdb28b682b7a87c66fb42f878
bairiyujinn <156569749+bairiyujinn@users.noreply.github.com>
2024-12-01 00:19:33 +0800

    Update main.go

--- Learning-Go/main.go
+++ Learning-Go/main.go
@@ -77,3 +77,71 @@ func main() {
 
 由以上两个例子发现可能存在的规律是：iota的计算规律决定之后所有的计算准则
 
+
+
+
+
+package main
+
+func main() {
+
+	strings := []string{"candy", "toy"}
+	for i, s := range strings {
+		fmt.Println(i, s)
+	}
+	输出：  0 candy
+	       1 toy
+	
+*****************************************************************************
+	numbers := [6]int{1, 2, 3, 5}
+	for i, x := range numbers {
+	fmt.Printf("第 %d 位 x 的值= %d\n", i, x)
+	}
+	输出：
+	
+		    第 0 位 x 的值 = 1
+		    第 1 位 x 的值 = 2
+		    第 2 位 x 的值 = 3
+		    第 3 位 x 的值 = 5
+		    第 4 位 x 的值 = 0
+		    第 5 位 x 的值 = 0
+******************************************************************************
+	map1 := make(map[int]float32)
+	map1[1] = 1.0
+	map1[2] = 2.0
+	map1[3] = 3.0
+
+	读取key和value
+		for key, value := range map1 {
+			fmt.Printf("key is:%d--value is: %f\n", key, value)
+		}
+
+		输出：
+		key is:1--value is: 1.000000
+	        key is:2--value is: 2.000000
+	        key is:3--value is: 3.000000
+	
+        ******************************************
+	for key := range map1 {
+		fmt.Printf("key is: %d\n", key)
+	}
+
+	 读取 key
+	
+		key is: 1
+		key is: 2
+		key is: 3
+		key is: 4
+        *******************************************
+	for _, value := range map1 {
+		fmt.Printf("value is: %f\n", value)
+	}
+	 读取 value
+	
+		value is: 4.000000
+		value is: 1.000000
+		value is: 2.000000
+		value is: 3.000000
+	
+}
+
